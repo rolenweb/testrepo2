1dca45a58a0e08d031fe414b65fd63cc25d637b2
rolenweb <rolenweb@yandex.ru>
2015-09-22 23:04:25 +0300

    second commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ echo "<!DOCTYPE html>
 </head>
 <body style=\"background: url(fon.png) top left repeat-x\">
 <center>
-<h1>TESTasdasdas</h1>
+<h1>TESTasdasdas222222222222222222222222222</h1>
 <br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 32px; color: green; font-family: Arial, Verdana; text-shadow: 0 1px 0 #fff\">Добро пожаловать в Open Server!</span>
 <br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Он работает ;-)</span>
 <br><img src=\"st.png\" style=\"margin: 40px 0\"><br><a href=\"http://open-server.ru/docs/\" style=\"font-size: 24px; color: #048acd; font-family: Arial;\">Руководство пользователя</a></span><br><br><br></div>
